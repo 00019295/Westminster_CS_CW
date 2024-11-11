bfb28f5c1bedff4ae0577fe73d259fc3337ebd85
00019295 <wiut00019295@gmail.com>
2024-11-11 14:26:17 +0500

    initial file creation

---
+++ src/Attendance.py
@@ -0,0 +1,4 @@
+class AttendanceRecord:
+    def __init__(self, student, status):
+        self.student = student
+        self.status = status
---
+++ src/Student.py
@@ -0,0 +1,5 @@
+class Student:
+    def __init__(self, student_id, full_name, grade):
+        self.student_id = student_id
+        self.full_name = full_name
+        self.grade = grade
